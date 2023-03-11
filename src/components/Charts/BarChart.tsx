86ab9c3cbcd3e13439fa4d6a4d9abc84eecde064
TranHuyHiep <tranhuyhiep0710@gmail.com>
2023-03-11 22:27:46 +0700

    config done

---
+++ src/components/Charts/BarChart.tsx
@@ -0,0 +1,11 @@
+// BarChart.tsx
+export {}; // thêm dòng này để đánh dấu tệp BarChart.tsx là một module
+import React from 'react';
+
+// định nghĩa component BarChart
+const BarChart = () => {
+  // ...
+  return <div>Bar chart</div>;
+};
+
+export default BarChart;
